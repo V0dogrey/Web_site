2ee76654b00d63c4a0e8063fa1a31b9e40897501
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-14 17:18:14 +0500

    make main page sample

--- WebSite/main/views.py
+++ WebSite/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+"""from django.http import HttpResponse"""
 
 # Create your views here.
 def index(requset):
-    return HttpResponse('<h1>Hello world!</h1>')
+    return render(requset, 'main/index.html')
