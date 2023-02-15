5a7c662d8cb261f4b9181ac0d2438c923e3f8aeb
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-15 20:34:10 +0500

    make named urls and header styles

--- WebSite/main/urls.py
+++ WebSite/main/urls.py
@@ -5,6 +5,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index),
-
+    path('', views.index, name='main'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- WebSite/news/urls.py
+++ WebSite/news/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.news)
+    path('', views.news, name='news')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
