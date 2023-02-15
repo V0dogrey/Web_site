87df01c90e4f2ea4ebe8b08af4ce2c523e1d6842
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-15 19:33:12 +0500

    back fix

--- WebSite/main/urls.py
+++ WebSite/main/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
