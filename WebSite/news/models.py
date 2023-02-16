4c32322a7fd217a74fc7e7ac8db884579093eed0
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-16 14:07:36 +0500

    create news model and prepare to migration

--- WebSite/news/models.py
+++ WebSite/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50)
+    anons = models.CharField('Анонс', max_length=250)
+    news_text = models.TextField('Статья')
+    date = models.DateTimeField('Дата публикации')
