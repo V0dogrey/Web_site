1b9cf37c6ed88ead8435b5cefed571b3ad18cd55
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-18 00:03:05 +0500

    update CommentList model and make 1 for many chain

--- WebSite/news/models.py
+++ WebSite/news/models.py
@@ -22,3 +22,10 @@ class CommentsList(models.Model):
     user_name = models.CharField('Имя', max_length=50)
     text = models.TextField('Комментарий', max_length=500)
     date = models.DateTimeField('Дата')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Кометарии"
