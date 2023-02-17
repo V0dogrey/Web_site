6745eae09171379d4a2aef9a593281a97d843ec4
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-17 14:31:49 +0500

    make page for every news

--- WebSite/news/views.py
+++ WebSite/news/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Articles
+from  django.views.generic import DetailView
 
 # Create your views here.
+
+class News_page(DetailView):
+    model = Articles
+    template_name = 'news/news_page.html'
+    context_object_name = 'article'
+
+
 def news(request):
     news = Articles.objects.order_by('-date')
     content = {
