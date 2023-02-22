ce844c2872446ea19482d09d3db0088a523665fa
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-23 01:17:34 +0500

    add pictures

--- WebSite/WebSite/settings.py
+++ WebSite/WebSite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_h*1^zz-5$kp_z^y=soofw$&2j=c(meyz4&k@z+&*0jbuc$@z+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- WebSite/news/models.py
+++ WebSite/news/models.py
@@ -8,6 +8,7 @@ class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     news_text = models.TextField('Статья')
+    picture = models.ImageField(upload_to="photos")
     date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
