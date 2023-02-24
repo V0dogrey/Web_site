ac66ef760fde68df5e8d1d22f30700cd911f79cb
V0dogrey <CanptanVodogrey@gmail.cm>
2023-02-24 16:27:45 +0500

    apd admin panel

--- WebSite/news/admin.py
+++ WebSite/news/admin.py
@@ -3,5 +3,15 @@ from .models import Articles, CommentsList
 
 # Register your models here.
 
-admin.site.register(Articles)
-admin.site.register(CommentsList)
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'anons')
+    list_display_links = ('title', 'id')
+    search_fields = ('id', 'title', 'news_text')
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'news_id', 'user_name', 'text')
+    list_display_links = ('id', 'news_id', 'user_name', 'text')
+    search_fields = ('user_name', 'text')
+
+admin.site.register(Articles, ArticlesAdmin)
+admin.site.register(CommentsList, CommentsAdmin)
--- WebSite/news/models.py
+++ WebSite/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -13,6 +14,9 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('news_id', kwargs={'pk': self.pk})
 
     class Meta:
         verbose_name = "Новость"
